b11d42255ce08c94a6697400b6ba4a345c537b01
Lucas do Rosario <lsdorosario@gmail.com>
2023-02-14 09:14:14 -0300

    Add operate function

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+" :
+            return add(a, b);
+        case "-" :
+            return subtract(a, b);
+        case "*" :
+            return multiply(a, b);
+        case "/" :
+            if (b == 0) {
+                return null;
+            } else {
+                return divide(a, b);
+            }
+        default: 
+        return null;
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
