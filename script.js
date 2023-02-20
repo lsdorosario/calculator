24c969d08c658be488a66182756eddef196907c1
Lucas do Rosario <lsdorosario@gmail.com>
2023-02-20 10:16:37 -0300

    Fix display width bug

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-populatesDisplay();
-
-// Creates variables for number display, operands and result
+// Defines number display
 let numberDisplay = document.querySelector("#num-display");
+
+// Creates variables for operand and result
 let operand1 = 0;
 let operand2 = 0;
 let result = 0;
+
+// Calculator 
+populatesDisplay();
+// opTest();
+
+// Operation test
+// function opTest() {
+//     const OPS_ARRAY = document.querySelectorAll(".operators");
+//     OPS_ARRAY.forEach(button => {
+//         button.addEventListener("click", () => {
+//             clearDisplay();
+
+//             result = operate(button.value, operand1, 2);
+//             numberDisplay.textContent = result;
+//         });
+//     });
+// }
 
 // Creates operand nodelist and adds event listener
 function populatesDisplay() {
@@ -12,28 +29,29 @@ function populatesDisplay() {
     NUM_ARRAY.forEach(button => {
         button.addEventListener("click", () => {
             addToDisplay(button.value);
+            return parseInt(numberDisplay.innerText);
         });
     });
-}
-
-function clearDisplay() {
-    numberDisplay.textContent = "";
 }
 
 function addToDisplay(btnValue) {
     numberDisplay.innerText = numberDisplay.innerText += btnValue;
 }
 
+function clearDisplay() {
+    numberDisplay.textContent = "";
+}
+
 // Uses basic arithmetic functions to calculate
 function operate(operator, a, b) {
     switch (operator) {
-        case "+" :
+        case "add" :
             return add(a, b);
-        case "-" :
+        case "subtract" :
             return subtract(a, b);
-        case "x" :
+        case "multiply" :
             return multiply(a, b);
-        case "%" :
+        case "divide" :
             if (b == 0) {
                 return null;
             } else {
